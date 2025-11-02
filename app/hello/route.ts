fbdd215140bd6bfbaa7af6e8ee9be981b585cad0
york2025 <york.chin@gmail.com>
2025-11-02 13:52:39 +0800

    Demo hello method

--- app/hello/route.ts
+++ app/hello/route.ts
@@ -1,3 +1,15 @@
 export async function GET(request: Request) {
-    return Response.json({ message: (await request.text()).toString() })
+    console.log(request.url)
+    const { searchParams } = new URL(request.url)
+    const queryParams: Record<string, string> = {}
+    for (const [key, value] of searchParams.entries()) {
+        queryParams[key] = value
+    }
+
+    return Response.json(queryParams)
+}
+
+export async function POST(request: Request) {
+    const body = await request.json()
+    return Response.json(body)
 }
