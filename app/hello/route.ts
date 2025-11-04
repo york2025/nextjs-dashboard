4675f98fdb697727dce4a7e010ab2fe9aa2246ad
york2025 <york.chin@gmail.com>
2025-11-04 10:42:33 +0800

    add more feature

--- app/hello/route.ts
+++ app/hello/route.ts
@@ -1,14 +1,33 @@
 import type { NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, {params}: {params: Promise<{team: string}>}) {
+export async function GET(request: NextRequest, { params }: { params: Promise<{ team: string }> }) {
     const searchParams = Object.fromEntries(request.nextUrl.searchParams);
     console.log(searchParams); // { name: 'york', age: '30' }
     return Response.json(searchParams);
 }
 
 
+// export async function GET() {
+//   return new Response(
+//     `<?xml version="1.0" encoding="UTF-8" ?>
+// <rss version="2.0">
+
+// <channel>
+//   <title>Next.js Documentation</title>
+//   <link>https://nextjs.org/docs</link>
+//   <description>The React Framework for the Web</description>
+// </channel>
+
+// </rss>`,
+//     {
+//       headers: {
+//         'Content-Type': 'text/xml',
+//       },
+//     }
+//   )
+// }
 
 export async function POST(request: NextRequest) {
     const body = await request.json()
-    return Response.json(body)
+    return Response.json({ body })
 }
